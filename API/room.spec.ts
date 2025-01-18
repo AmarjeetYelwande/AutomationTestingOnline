4a9b4b9d7711c003aa152d40b6d82ee331c35d1c
Amarjeet Yelwande <yelwande@gmail.com>
2025-01-18 21:28:05 +0000

    Assert for room number 1 only

--- API/room.spec.ts
+++ API/room.spec.ts
@@ -3,10 +3,10 @@ import { createRequire } from 'node:module';
 const room = createRequire(import.meta.url)("./Data/room.json");
 
 test('should be able to create a booking', async ({ request }) => {
-    const response = await request.get("/room")
+    const response = await request.get("/room/1")
     console.log(await response.json());
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
     const responseBody = await response.json()
-    expect(JSON.stringify(responseBody)).toEqual(JSON.stringify(room))
+    expect(JSON.stringify(responseBody)).toContain(JSON.stringify(room))
 });
