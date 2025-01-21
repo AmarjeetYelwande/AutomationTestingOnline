381300b7c7e815906d825862d61e021fc9d31df2
Amarjeet Yelwande <yelwande@gmail.com>
2025-01-21 10:11:21 +0000

    Taking backup of work done so far

--- API/room.spec.ts
+++ API/room.spec.ts
@@ -1,12 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { createRequire } from 'node:module';
 const room = createRequire(import.meta.url)("./Data/room.json");
+const schema = createRequire(import.meta.url)("./Data/schema.json");
+import { Ajv } from 'ajv';
+const ajv = new Ajv();
+ajv.addKeyword("optional");
 
 test('should be able to create a booking', async ({ request }) => {
-    const response = await request.get("/room/1")
-    console.log(await response.json());
+    const response = await request.get("/room/1");
+    const responseBody = await response.json();
+    console.log(responseBody);
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
-    const responseBody = await response.json()
-    expect(JSON.stringify(responseBody)).toContain(JSON.stringify(room))
+    expect(JSON.stringify(responseBody)).toContain(JSON.stringify(room));
+    const isvalid = ajv.validate(schema, responseBody);
+    if (!isvalid) console.log(ajv.errors);
+    expect(isvalid).toBeTruthy();
 });
+
