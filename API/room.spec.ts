a1ed85e57bda8c417f6534334dfc5d87e8956d03
Amarjeet Yelwande <yelwande@gmail.com>
2025-01-21 11:30:30 +0000

    Fixed API Test. Remove not required tests

--- API/room.spec.ts
+++ API/room.spec.ts
@@ -1,20 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { createRequire } from 'node:module';
-const room = createRequire(import.meta.url)("./Data/room.json");
 const schema = createRequire(import.meta.url)("./Data/schema.json");
 import { Ajv } from 'ajv';
 const ajv = new Ajv();
-ajv.addKeyword("optional");
 
-test('should be able to create a booking', async ({ request }) => {
-    const response = await request.get("/room/1");
+test('Validate schema of the room', async ({ request }) => {
+    const response = await request.get("/room");
     const responseBody = await response.json();
-    console.log(responseBody);
+    const firstElement = responseBody.rooms[0];
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
-    expect(JSON.stringify(responseBody)).toContain(JSON.stringify(room));
-    const isvalid = ajv.validate(schema, responseBody);
-    if (!isvalid) console.log(ajv.errors);
-    expect(isvalid).toBeTruthy();
+    const isSchemaValid = ajv.validate(schema, firstElement);
+    if (!isSchemaValid) console.log(ajv.errors);
+    expect(isSchemaValid).toBeTruthy();
 });
 
